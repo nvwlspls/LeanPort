6f69c666c525438661fb0b90a7f8afd04d8820ff
Wayne Jessen <wjessen@traconpharma.com>
2020-07-21 22:02:31 -0700

    moved stopwatch to own file

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # This page is meant to be a skill building practice and a way to start form the ground up.
 
-## The first thing I created was a calculator in vanilla javascript.  Nothing too impressive but it helped me learn more about javascript.
+## The first thing I created was a calculator in vanilla javascript.  It was a great way to get mroe comfortable with javascript.
---
+++ static/js/stopWatch.js
@@ -0,0 +1,101 @@
+ function createStopWatch(element){
+    // createStopWatch will create a stopwtach display as the child of the given
+    // element.
+    function addHtmlToElement(element){
+        heading_el = document.createElement("h1")
+        heading_text = document.createTextNode("Stop Watch")
+
+        element.appendChild(heading_el)
+        heading_el.appendChild(heading_text)
+
+
+
+        //     <h1 class="">Stopwatch</h1>
+        // <div class="row">
+        //     <h2>HH:MM:SS,S</h2>
+        // </div>
+        // <h2 class="watchEl"id="curTimer">99:99:99,9</h2>
+    }
+
+    addHtmlToElement(element);  
+
+
+    // //  https://gist.github.com/vankasteelj/74ab7793133f4b257ea3
+    //     function sec2time(timeInSeconds) {
+    //         var pad = function(num, size) { return ('000' + num).
+    //                 slice(size * -1); },
+    //         time = parseFloat(timeInSeconds).toFixed(3),
+    //         hours = Math.floor(time / 60 / 60),
+    //         minutes = Math.floor(time / 60) % 60,
+    //         seconds = Math.floor(time - minutes * 60),
+    //         milliseconds = time.slice(-3);
+
+    //         return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + 
+    //             pad(seconds, 2) + ',' + pad(milliseconds, 1);
+    //     }
+
+    //     function updateStoppedTimers (stoppedTimersList, listOfTimers){
+
+    //         // create new list item
+    //         var listItem = document.createElement("li");
+    //         listItem.className = "stoppedTimer";
+    //         var text = document.createTextNode(stoppedTimersList[0]);
+    //         listItem.appendChild(text);
+            
+    //         // add list item to html list
+    //         listOfTimers.prepend(listItem);
+
+    //         // list items
+    //         listItems = document.getElementsByClassName("stoppedTimer");
+            
+    //         // if the list is over 5 long 
+    //         while (listItems.length > 5) {
+    //                 listItems[5].remove();
+    //             }
+    //         }
+
+    //     var finishedTimers = []
+
+    //     var timerList = document.getElementById("stoppedTimersList")
+
+    //     var mainStopWatch = {
+    //         watchTimeText: document.getElementById("curTimer"),
+    //         running: false
+    //     }
+
+    //     //add event listener to startButton
+    //     document.getElementById("startButton").
+    //         addEventListener("click", function (event){
+                
+    //             mainStopWatch.startDate = new Date();
+    //             mainStopWatch.running = true;
+            
+    //         })
+        
+    //     document.getElementById("stopButton").
+    //         addEventListener("click", function (params) {
+            
+    //         stopTimer = mainStopWatch.watchTime
+    //         mainStopWatch.running = false;
+    //         finishedTimers.unshift(stopTimer)
+    //         finishedTimers = finishedTimers.slice(0,4)
+    //         updateStoppedTimers(finishedTimers, timerList)
+
+    //     });
+    
+    //     window.setInterval(() => {
+
+    //         if (mainStopWatch.running) {
+                
+    //             // if the main stopwatch is running update the timer
+    //             var nowDate = new Date();
+    //             diffTime = nowDate.getTime() - 
+    //                     mainStopWatch.startDate.getTime();
+    //             watchTime =   sec2time(diffTime / 1000);
+    //             mainStopWatch.watchTime = watchTime
+    //             mainStopWatch.watchTimeText.innerHTML = 
+    //                 mainStopWatch.watchTime 
+    //         }
+
+    //     }, 10);
+};
